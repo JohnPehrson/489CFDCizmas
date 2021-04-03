bb5e45534c1b122a65a732fb7443d806973fea6c
JohnPehrson <clark.pehrson@tamu.edu>
2021-04-03 13:41:34 -0500

    Function to refresh f,g in the time looping

---
+++ refresh_f_g.m
@@ -0,0 +1,15 @@
+function [f,g] = refresh_f_g(q,user_Gamma)
+%This function refreshes the cellular f,g vectors given an input vector q
+f = zeros(4,1);
+g = f;
+
+rho = q(1);
+u = q(2)/q(1);
+v = q(3)/q(1);
+E = q(4)/q(1);
+p = (user_Gamma-1)*rho*(E-0.5*(u^2+v^2));
+
+f = [rho*u;rho*u^2+p;rho*u*v;rho*(E+p)*u];
+g = [rho*v;rho*u*v;rho*v^2+p;rho*(E+p)*v];
+end
+
