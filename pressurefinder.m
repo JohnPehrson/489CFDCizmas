07a40afef67b837bd41b400661fe502b3846ed0a
John C Pehrson <clark.pehrson@tamu.edu>
2021-04-13 17:17:22 -0500

    Now finds ptotal from pstatic

--- pressurefinder.m
+++ pressurefinder.m
@@ -1,9 +1,10 @@
-function [Ps] = pressurefinder(P_resevoir,user_Mach,user_Gamma)
+function [Ptotal] = pressurefinder(user_Mach,user_Gamma,Pstatic)
 %This function calculates the static pressure in a subsonic compressible
 %flow given a resevoir (stagnation) pressure, local mach number, and gamma
 
 %This function returns the static pressure at the mach number
 
-Ps = P_resevoir/(1+((user_Gamma-1)/2)*user_Mach^2)^((user_Gamma)/(user_Gamma-1));
+Ptotal = Pstatic*(1+((user_Gamma-1)/2)*user_Mach^2)^((user_Gamma)/(user_Gamma-1));
+
 end
 
