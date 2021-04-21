b54b218eeb79cd5fc1f9a7197af35a3923f5d03e
John C Pehrson <54759417+JohnPehrson@users.noreply.github.com>
2021-04-21 11:00:04 -0500

    Make wall BC use Pressure like the other BC

--- applyBC.m
+++ applyBC.m
@@ -1,8 +1,8 @@
 function [cells_q,cells_f,cells_g] = applyBC(nodes_x,nodes_y,user_alpha,user_Gamma,user_Mach,P_static,cells_q,cells_f,cells_g,cells_Imax,cells_Jmax)
 %This function contains the 4 boundary condition functions. Call this
 %function to refresh all boundary conditions. 
-    [cells_q,cells_f,cells_g] = applyUpperWallBC(nodes_x,nodes_y,cells_q,cells_f,cells_g,cells_Imax,cells_Jmax);
-    [cells_q,cells_f,cells_g] = applyBottomWallBC(nodes_x,nodes_y,cells_q,cells_f,cells_g,cells_Imax);
+    [cells_q,cells_f,cells_g] = applyUpperWallBC(user_Gamma,nodes_x,nodes_y,cells_q,cells_f,cells_g,cells_Imax,cells_Jmax);
+    [cells_q,cells_f,cells_g] = applyBottomWallBC(user_Gamma,nodes_x,nodes_y,cells_q,cells_f,cells_g,cells_Imax);
     [cells_q,cells_f,cells_g] = applyOutletBC(user_Gamma,user_Mach,cells_q,cells_f,cells_g,cells_Imax,cells_Jmax,P_static);
     [cells_q,cells_f,cells_g] = applyInletBC(user_alpha,user_Gamma,user_Mach,P_static,cells_q,cells_f,cells_g,cells_Imax,cells_Jmax);
 end
