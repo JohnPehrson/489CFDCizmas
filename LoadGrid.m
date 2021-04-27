3b6c4aca7dfcbb61b27c82bb06f8d305270bed11
John C Pehrson <54759417+JohnPehrson@users.noreply.github.com>
2021-04-27 18:35:56 -0500

    Loading for new grids

--- LoadGrid.m
+++ LoadGrid.m
@@ -2,22 +2,19 @@ function [xout,yout] = LoadGrid(gridtype)
 %Loads the laplace grid based on the input type defined by the user
 
     if strcmp(gridtype,'coarse') %coarsegrid
-        load('coarsegridX.mat');
-        load('coarsegridY.mat');
+        load('coarselaplace_gridX.mat');
+        load('coarselaplace_gridY.mat');
     elseif strcmp(gridtype,'medium') %medium grid
-        load('mediumgridX.mat');
-        load('mediumgridY.mat');
-    elseif strcmp(gridtype,'fine')
-        %fine grid
-        
+        load('mediumlaplace_gridX.mat');
+        load('mediumlaplace_gridY.mat');
+    elseif strcmp(gridtype,'fine')  %fine grid
+        load('finelaplace_gridX.mat');
+        load('finelaplace_gridY.mat');
     elseif strcmp(gridtype,'testalg') %test grid
-        load('51_10_alg_gridX.mat');
-        load('51_10_alg_gridY.mat');
+        load('testgridX.mat');
+        load('testgridY.mat');
         xout = x;
         yout = y;
-   elseif strcmp(gridtype,'testlaplace') %test grid
-        load('51_10_laplace_gridX.mat');
-        load('51_10_laplace_gridY.mat');
     else
         fprintf('Please input the correct grid descriptor');
     end
